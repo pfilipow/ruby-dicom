b323477dc4f99453c387ceee5ee397db89d293e4
Paul Filipow <paul@filipow.ca>
2023-03-13 15:17:43 -0600

    wip: save individual files

--- lib/dicom/link.rb
+++ lib/dicom/link.rb
@@ -455,10 +455,12 @@ module DICOM
 
       # Reset command results arrays:
       @command_results = Array.new
-      @data_results = Array.new
       file_transfer_syntaxes = Array.new
       files = Array.new
       single_file_data = Array.new
+      # single_file_data = Tempfile.new('dimse', binmode: true)
+      # single_file_data = File.new('dimse.dcm', 'ab')
+      transfer_syntax = nil
 
       @listen = true
       # segments = Array.new
@@ -468,34 +470,32 @@ module DICOM
 
       while @listen
         segments = receive_single_transmission(@min_length, true)
-        logger.info "SEGMENTS #{segments.length}"
         # Proceed to extract data from the captured segments:
         segments&.each do |info|
           if info[:valid]
             # Determine if it is command or data:
             if info[:presentation_context_flag] == DATA_MORE_FRAGMENTS
-              logger.info "DATA_MORE_FRAGMENTS"
-              @data_results << info[:results]
               single_file_data  << info[:bin]
+
             elsif info[:presentation_context_flag] == DATA_LAST_FRAGMENT
-              logger.info "DATA_LAST_FRAGMENT"
-              @data_results << info[:results]
               single_file_data  << info[:bin]
               # Join the recorded data binary strings together to make a DICOM file binary string and put it in our files Array:
-              files << single_file_data.join
-              single_file_data = Array.new
+              # files << single_file_data.join
+              @file_handler.receive_file(single_file_data.join, transfer_syntax)
+              single_file_data.clear
+
             elsif info[:presentation_context_flag] == COMMAND_LAST_FRAGMENT
-              logger.info "COMMAND_LAST_FRAGMENT"
               @command_results << info[:results]
               @presentation_context_id = info[:presentation_context_id] # Does this actually do anything useful?
-              file_transfer_syntaxes << @presentation_contexts[info[:presentation_context_id]]
+              transfer_syntax = @presentation_contexts[info[:presentation_context_id]]
+              file_transfer_syntaxes << transfer_syntax
             end
           end
         end
       end
       # Process the received files using the customizable FileHandler class:
-      success, messages = @file_handler.receive_files(path, files, file_transfer_syntaxes)
-      return success, messages
+      # success, messages = @file_handler.receive_files(path, files, file_transfer_syntaxes)
+      # return success, messages
     end
 
     # Handles the rejection message (The response used to an association request when its formalities are not correct).
@@ -1097,7 +1097,6 @@ module DICOM
         # Receive data and append the current data to our segments array, which will be returned.
         data = receive_transmission(@min_length)
         current_segments = interpret(data, file)
-        logger.info "DATA_TX: Recvievd #{data.length} bytes in #{current_segments.length} segments"
         if current_segments
           current_segments.each do |cs|
             segments << cs
@@ -1115,7 +1114,6 @@ module DICOM
       data = receive_transmission(min_length)
       segments = interpret(data, file)
       segments << {:valid => false} unless segments.length > 0
-      logger.info "SINGLE_TX: Recvievd #{data.length} bytes in #{segments.length} segments"
       return segments
     end
 
